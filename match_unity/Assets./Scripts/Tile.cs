519161ffdf2f2dea30dca1f70e1bf25acd1daec8
schizofunky <scott_mather@hotmail.com>
2014-01-05 14:23:16 +0000

    Got recursive matching working

---
+++ match_unity/Assets./Scripts/Tile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile {
+	
+	public int tileContent {get; private set;}
+
+	public GameObject gameObject {get; private set;}
+
+	private bool _selected = false;
+
+
+	public Tile(int id, Sprite sprite, int tileIndex) {
+		gameObject = new GameObject();
+		gameObject.name = "Tile"+id;
+		gameObject.AddComponent("SpriteRenderer");
+		gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+		tileContent = tileIndex;
+	}
+
+	~Tile(){
+		//Debug.Log("Tile was destroyed");
+	}
+
+	public void SetPosition(float x, float y){
+		gameObject.transform.position = new Vector3(x,y,1);
+		gameObject.transform.localScale = new Vector3(2,2,1);
+	}
+
+	public bool HitTest(float x, float y){
+		bool hit = false;
+		Vector3 pos = gameObject.transform.position;
+		float size = 0.4f;
+		if(x > pos.x - size &&  x < pos.x + size){
+			if(y > pos.y - size &&  y < pos.y + size){
+				hit = true;
+			}
+		}
+		return hit;
+	}
+
+	public void ToggleSelected(){
+		_selected = !_selected;
+		if(_selected){			
+			gameObject.transform.localScale = new Vector3(2.2f,2.2f,1);
+		}else{
+			gameObject.transform.localScale = new Vector3(2,2,1);
+		}
+	}
+}
---
+++ match_unity/Assets./Scripts/TileAnimation.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileAnimation {
+
+	public const int TRANSFORM = 0;
+	public const int SCALE = 1;
+	private bool _completed = false;
+	private int _iterations = 0;
+	private int _time;
+	private Vector3 _startPoint;
+	private Vector3 _endPoint;
+	private Vector3 _updateValues;
+	private GameObject _tile;
+	private int _type;
+
+	public TileAnimation(GameObject tile, Vector3 endPoint,int time,int type){
+		_endPoint = endPoint;
+		_type = type;
+		_tile = tile;
+		_time = time;
+		_startPoint = GetStartValue();
+		_updateValues = CalculateDistancePerTick();
+	}
+
+	public void UpdateAnimation(){
+		if(!_completed){
+			_iterations++;
+			switch(_type){
+			case TRANSFORM:
+				_tile.transform.Translate(_updateValues.x,_updateValues.y, 0);
+				break;
+			case SCALE:
+				float uniformScale = _tile.transform.localScale.x+_updateValues.x;
+				_tile.transform.localScale = new Vector3(uniformScale,uniformScale,1);
+				break;
+			}
+			if(_iterations >= _time){
+				_completed = true;
+			}
+		}
+	}
+
+	public bool IsCompleted(){
+		return _completed;
+	}
+
+	public void ReverseAnimation(){
+		_completed = false;
+		_iterations = 0;
+		_endPoint = _startPoint;
+		_startPoint = GetStartValue();
+		_updateValues = CalculateDistancePerTick();
+	}
+
+	private Vector3 CalculateDistancePerTick(){
+		Vector3 difference = _endPoint - _startPoint;
+		difference.x = difference.x/_time;
+		difference.y = difference.y/_time;
+		return difference;
+	}
+
+	private Vector3 GetStartValue(){
+		Vector3 value;
+		switch(_type){
+		case TRANSFORM:
+			value = _tile.transform.position;
+			break;			
+		case SCALE:
+			value = _tile.transform.localScale;
+			break;
+		default:
+			value = _tile.transform.position;
+			break;
+		}
+		return value;
+	}
+
+}
